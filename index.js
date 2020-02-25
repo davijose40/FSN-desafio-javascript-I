ab18f1c71d143f76112c69085d47bfa0edf0d755
davijose40 <davijose40@gmail.com>
2020-02-25 11:27:21 -0300

    aprovarAluno finalizada

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
 // Base a ser utilizada
-const alunosDaEscola=[
-    {nome:"Henrique",notas:[],cursos:[],faltas:5},
-    {nome:"Edson",notas:[],cursos:[],faltas:2},
-    {nome:"Bruno",notas:[10,9.8,9.6],cursos:[],faltas:0},
-    {nome:"Guilherme",notas:[10,9.8,9.6],cursos:[{nomeDoCurso:"Full Stack",dataMatricula:new Date}],faltas:0},
-    {nome:"Carlos",notas:[],cursos:[],faltas:0},
-    {nome:"Lucca",notas:[10,9.8,9.6],cursos:[{nomeDoCurso:"UX",dataMatricula:new Date}],faltas:0}
+const alunosDaEscola = [{
+        nome: "Henrique",
+        notas: [],
+        cursos: [],
+        faltas: 5
+    },
+    {
+        nome: "Edson",
+        notas: [],
+        cursos: [],
+        faltas: 2
+    },
+    {
+        nome: "Bruno",
+        notas: [10, 9.8, 9.6],
+        cursos: [],
+        faltas: 0
+    },
+    {
+        nome: "Guilherme",
+        notas: [10, 9.8, 9.6],
+        cursos: [{
+            nomeDoCurso: "Full Stack",
+            dataMatricula: new Date
+        }],
+        faltas: 0
+    },
+    {
+        nome: "Carlos",
+        notas: [],
+        cursos: [],
+        faltas: 0
+    },
+    {
+        nome: "Lucca",
+        notas: [10, 9.8, 9.6],
+        cursos: [{
+            nomeDoCurso: "UX",
+            dataMatricula: new Date
+        }],
+        faltas: 0
+    }
 ];
 
 
@@ -23,19 +58,19 @@ function adicionarAluno(nome) {
 };
 
 function listarAlunos() {
-    
+
     //lista objeto aluno no console.
     console.log("==========Lista dos Alunos==========")
     console.log(" ")
     alunosDaEscola.forEach(element => {
-       
+
         console.log(`Nome: ${element.nome}`);
-        if(element.notas > 0) {
+        if (element.notas > 0) {
             console.log(`Nota: ${element.notas}`);
         } else {
             console.log("Não existem notas lançadas");
         }
-        if(element.cursos.length > 0) {
+        if (element.cursos.length > 0) {
             element.cursos.forEach(el => {
                 console.log(`Nome do Curso: ${el.nomeDoCurso}`);
                 console.log(`Data da Matricula: ${el.dataMatricula}`);
@@ -61,8 +96,8 @@ function buscarAluno(nome) {
 
 };
 
-function matricularAluno(aluno, curso){
-    
+function matricularAluno(aluno, curso) {
+
     //busca string aluno e adiciona curso e new Date
     let verificaSeExisteAluno = buscarAluno(aluno);
     let matriculaNovCurso = {
@@ -70,28 +105,62 @@ function matricularAluno(aluno, curso){
         dataMatricula: new Date
     };
 
-    if(verificaSeExisteAluno) {
+    if (verificaSeExisteAluno) {
         let aluno = verificaSeExisteAluno;
         aluno.cursos.push(matriculaNovCurso)
         console.log(`Aluno ${aluno.nome} matriculado no curso ${matriculaNovCurso.nomeDoCurso} com sucesso.`);
     } else {
-        console.log("Não foi possivel fazer a matricula, aluno não encontrado.")
+        console.log(`Não foi possivel atribuir falta, aluno não encontrado.`);
     }
 
 };
 
-function aplicarFalta(aluno){
+function aplicarFalta(aluno) {
 
     //busca usando objeto aluno incrementa falta de um sempre que chamada
-    let verificaSeExisteAluno = buscarAluno(aluno.nome);
-    if(verificaSeExisteAluno) {
+    let verificaSeExisteAluno = buscarAluno(aluno);
+    if (verificaSeExisteAluno) {
         let aluno = verificaSeExisteAluno;
         aluno.faltas++;
         console.log(`Aluno ${aluno.nome} teve acrescida uma falta totalizando: ${aluno.faltas}.`);
     } else {
-        console.log("Não foi possivel atribuir falta, aluno não encontrado.");
+        console.log(`Não foi possivel atribuir falta, aluno: ${aluno.nome} não encontrado.`);
     }
 
+};
+
+function aplicarNota(aluno, nota) {
+
+    // busca usando objeto aluno atribui uma nota no array notas, 
+    // obs: qual nota? deveria ser passada como parametro.
+    let verificaSeExisteAluno = buscarAluno(aluno);
+    if (verificaSeExisteAluno) {
+        let aluno = verificaSeExisteAluno;
+        aluno.notas.push(nota);
+        console.log(`Aluno ${aluno.nome} teve acrescida uma nota: ${aluno.notas}.`);
+    } else {
+        console.log(`Não foi possivel atribuir falta, aluno: ${aluno.nome} não encontrado.`);
+    }
+
+};
+
+function aprovarAluno(aluno) {
+
+    // verifica notas e faltas para aprovar aluno
+    let verificaSeExisteAluno = buscarAluno(aluno);
+    if (verificaSeExisteAluno) {
+        let aluno = verificaSeExisteAluno;
+        let media = aluno.notas.length > 0 
+            ? aluno.notas.reduce((ac, el) => ac += el) / aluno.notas.length 
+            : 0;
+        if (aluno.faltas <= 3 && media >= 7) {
+            console.log(`${aluno.nome} foi aprovado com media: ${media} e total de ${aluno.faltas} faltas.`);
+        } else {
+            console.log(`${aluno.nome} foi reprovado com media: ${media} e total de ${aluno.faltas} faltas.`);
+        }
+    } else {
+        console.log(`aluno: ${aluno.nome} não encontrado.`);
+    }
 };
 
 
@@ -100,5 +169,7 @@ function aplicarFalta(aluno){
 //console.log(listarAlunos());
 //console.log(buscarAluno('Lucca'));
 //console.log(matricularAluno('Lucca', "javascript"));
-console.log(aplicarFalta({nome:"Edson",notas:[],cursos:[],faltas:2}));
+//console.log(aplicarFalta("Edson"));
+//console.log(aplicarNota("Edson", 5));
 
+console.log(aprovarAluno("Bruno"));
